984528797d2470f8285c21c0f942fbaa2fa7624e
Silviyanancy <jsilviyanancy@gmail.com>
2024-12-08 21:24:23 -0600

    Adding backend

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["personal_details"]
+users_collection = db["users"]
+
+# Routes
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = list(users_collection.find({}, {"_id": 0}))
+    return jsonify(users), 200
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.json
+    users_collection.insert_one(data)
+    return jsonify({"message": "User added successfully!"}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
